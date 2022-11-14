f086dc8e709352ad4385b3d6da59304fafdf8118
pizsd <zerduo@126.com>
2022-11-14 10:51:43 +0800

    remove test code

--- app/cmd/play.go
+++ app/cmd/play.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
-	"strconv"
 )
 
 var CmdPlay = &cobra.Command{
@@ -14,7 +12,4 @@ var CmdPlay = &cobra.Command{
 
 func runPlay(cmd *cobra.Command, args []string) {
 	// 测试代码
-	var i int64 = 1
-	s := strconv.FormatInt(i, 10)
-	fmt.Printf("%T - %[1]s\n", s)
 }
